e1b2296deb5ef7268a6ed5f8868e27ff913651ae
Eric Doering <ericbenjamindoering@gmail.com>
2024-07-31 21:58:33 -0400

    adding tests for TermsAndConditions component

---
+++ tests/components/TermsAndConditions.test.tsx
@@ -0,0 +1,33 @@
+import { render, screen } from "@testing-library/react";
+import TermsAndConditions from "../../src/components/TermsAndConditions";
+import userEvent from "@testing-library/user-event";
+
+describe('Terms and Conditions', () => {
+    it('should render with correct text and initial state', () => {
+        render(<TermsAndConditions />);
+
+        const heading = screen.getByRole('heading');
+        expect(heading).toBeInTheDocument();
+        expect(heading).toHaveTextContent('Terms & Conditions');
+
+        const checkbox = screen.getByRole('checkbox');
+        expect(checkbox).toBeInTheDocument();
+        expect(checkbox).not.toBeChecked();
+
+        const button = screen.getByRole('button');
+        expect(button).toBeInTheDocument();
+        expect(button).toHaveTextContent(/submit/i);
+        expect(button).toBeDisabled();
+
+    })
+
+    it('should enable to submit button when the checkbox is checked', async () => {
+        render(<TermsAndConditions />);
+
+        const checkbox = screen.getByRole('checkbox');
+        const user = userEvent.setup();
+        await user.click(checkbox);
+
+        expect(screen.getByRole('button')).toBeEnabled();
+    })
+})
