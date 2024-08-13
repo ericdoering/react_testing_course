c3f027715bf255a79ccd6a3ff5dbe56dbe783978
Eric Doering <ericbenjamindoering@gmail.com>
2024-08-12 21:51:39 -0400

    successful testing of collapsible text component

--- src/pages/PlaygroundPage.tsx
+++ src/pages/PlaygroundPage.tsx
@@ -1,7 +1,13 @@
+import ExpandableText from "../components/ExpandableText";
 import Onboarding from "../components/Onboarding";
 
 const PlaygroundPage = () => {
-  return <Onboarding />;
+
+  return <ExpandableText text="Testing the test involves testing. When testing text it is important that actions are taken to
+  test text. When using testing frameworks to test text there maybe certain protocols in order to test the text. Here
+  is an example of text that is able to expand and be testable. We will run this through a test and see if the expandable text
+  is successful in the tests. I am writing this long paragraph in order to see if the testable text is able to succeed when it
+  is being tested." />;
 };
 
 export default PlaygroundPage;
